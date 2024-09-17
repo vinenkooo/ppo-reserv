52df287a1ef80cb178f49046d2b997778b9d7920
vinenkooo <vikky.mirr@gmail.com>
2024-09-17 13:49:20 +0700

    hello in file

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"os"
 )
 
 func main() {
 	helloString := "Hello world!"
-	//
-	//
-	fmt.Println(helloString)
+	file, _ := os.Create("hello.txt")
+	defer file.Close()
+	_, _ = file.WriteString(helloString)
 
 }
